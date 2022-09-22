011dd4b3b5e9e5d48637507b0166a023e2e43ec0
Marcos Henrique <marcos.henriquerds@gmail.com>
2022-09-22 17:57:43 -0300

    feat: create folder architecture

--- src/main/factories/decorators/authorize-http-client-decorator-factory.ts
+++ src/main/factories/decorators/authorize-http-client-decorator-factory.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from 'data/protocols/http'
+import { AuthorizeHttpClientDecorator } from 'main/decorators'
 import { makeLocalStorageAdapter } from 'main/factories/cache'
-import { AuthorizeHttpClientDecorator } from 'main/factories/decorators'
 import { makeAxiosHttpClient } from 'main/factories/http'
 
 export const makeAuthorizeHttpClientDecorator = (): HttpClient =>
